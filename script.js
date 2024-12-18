8c394e2f839e57b642c4ebe5eef36caa599bbab0
mohamedr5z9 <mohamedtark102@gmail.com>
2024-12-18 16:50:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example of a simple JavaScript function to show an alert when the button is clicked
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', function() {
+        alert('Welcome to my portfolio!');
+    });
+});
