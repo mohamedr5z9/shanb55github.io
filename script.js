9e5447917241d44e14d476b95d37c43458b24d79
mohamedr5z9 <mohamedtark102@gmail.com>
2024-12-18 18:01:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
+import ReactDOM from 'react-dom';
+import App from './App'
+import './index.css'
 
-// Smooth scrolling for navigation links
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
-
-        targetElement.scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-// Add an event listener for the scroll event
-window.addEventListener('scroll', () => {
-    const nav = document.querySelector('nav');
-    if (window.scrollY > 50) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
-});
-
-// Alert on page load (optional)
-window.onload = function() {
-    console.log("Welcome to my portfolio!");
-};
+ReactDOM.render(<App />, document.querySelector("#root"))
