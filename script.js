03bb552198245b7dcd0a955e09c27f0f380c1a4f
mohamedr5z9 <mohamedtark102@gmail.com>
2024-12-18 17:35:09 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Example of a simple JavaScript function to show an alert when the button is clicked
-document.addEventListener('DOMContentLoaded', function() {
-    const btn = document.querySelector('.btn');
-    btn.addEventListener('click', function() {
-        alert('Welcome to my portfolio!');
-    });
-});
