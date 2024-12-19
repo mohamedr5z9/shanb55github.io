7d2aa930654805d2f64a8c8c3057bfbe153433bf
mohamedr5z9 <mohamedtark102@gmail.com>
2024-12-19 11:57:04 +0200

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -33,22 +33,22 @@ const Contact = () => {
                     <article className="contact__option">
                         <HiOutlineMail className='contact__option-icon' />
                         <h4>Email</h4>
-                        <h5>tanvunguyen2205@gmail.com</h5>
+                        <h5>mohamedtark102@gmail.com</h5>
                         <a href="mailto:tanvunguyen2205@gmail.com" target="_blank">Send a message</a>
                     </article>
 
                     <article className="contact__option">
                         <RiMessengerLine className='contact__option-icon' />
                         <h4>Messenger</h4>
-                        <h5>Nguyen Vu</h5>
-                        <a href="https://m.me/vunguyen260803.trying" target="_blank" >Send a message</a>
+                        <h5>mohamed tarek</h5>
+                        <a href="="_blank" >Send a message</a>
                     </article>
 
                     <article className="contact__option">
                         <TbBrandLinkedin className='contact__option-icon' />
                         <h4>Linkedin</h4>
                         <h5>Nguyen Vu</h5>
-                        <a href="https://www.linkedin.com/in/nguyenvu2608/" target="_blank">Send a message</a>
+                        <a href="https://www.linkedin.com/in/netbuster/" target="_blank">Send a message</a>
                     </article>
                 </div>
                 {/* End of contact options */}
@@ -56,7 +56,7 @@ const Contact = () => {
                 <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name='name' placeholder='Raymond Rhodes' required />
                     <input type="email" name='email' placeholder='example@example.com' required />
-                    <textarea name='message' rows="7" placeholder='Hi Vu, I have a question...' required></textarea>
+                    <textarea name='message' rows="7" placeholder='Hi mohamed tarek, I have a question...' required></textarea>
                     <button type='submit' className='btn btn-primary'>Send Message</button>
                 </form>
             </div>
