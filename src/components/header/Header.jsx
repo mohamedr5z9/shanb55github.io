10d3eeb776feec1ef7172d956814504312e0efd2
mohamedr5z9 <mohamedtark102@gmail.com>
2024-12-19 12:06:22 +0200

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,8 +9,8 @@ const header = () => {
         <header>
             <div className="container header__container">
                 <h5> Hello I'm</h5>
-                <h1>Nguyen Tan Vu</h1>
-                <h5 className="text-light">Fresher Security Engineer</h5>
+                <h1>mohamed tarek</h1>
+                <h5 className="text-light">cyber  Security Engineer</h5>
                 <CTA />
                 <HeaderSocials />
 
